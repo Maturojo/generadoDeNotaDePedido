580aa1b72d0c0c3572316df1d06581c7227aed5e
Maturojo <maturojo123mdq@gmail.com>
2025-07-22 19:31:41 -0300

    Estoy corriguendo el fetch de apl

--- Frontend/js/config.js
+++ Frontend/js/config.js
@@ -1,4 +1,7 @@
+const isLocalhost = window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1";
 
-window.API_URL = window.location.hostname === "localhost"
-  ? "http://localhost:3000" // backend local
-  : "https://generadodenotadepedido.onrender.com"; // backend en Render
+const API_URL = isLocalhost
+    ? "http://localhost:3000" // Para desarrollo local
+    : "https://generadodenotadepedido.onrender.com"; // URL de tu backend en Render
+
+console.log("Usando API_URL:", API_URL);
